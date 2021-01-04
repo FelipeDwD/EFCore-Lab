de409e8cd121aae34203fc778734d75566ae7d71
Felipe Arruda <felipeneves089@gmail.com>
2021-01-04 19:40:17 -0300

    create value objects

---
+++ Dwd.FreeProjects.OrderDrawer/ValueObjects/AsFreight.cs
@@ -0,0 +1,9 @@
+namespace Dwd.FreeProjects.OrderDrawer.ValueObjects
+{
+    public enum AsFreight
+    {
+        CIF,
+        FOB,
+        NoFreight
+    }
+}
---
+++ Dwd.FreeProjects.OrderDrawer/ValueObjects/AsProduct.cs
@@ -0,0 +1,9 @@
+namespace Dwd.FreeProjects.OrderDrawer.ValueObjects
+{
+    public enum AsProduct
+    {
+        MerchandiseToResale,
+        Packing,
+        Service
+    }
+}
